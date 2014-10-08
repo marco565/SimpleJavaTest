d5845f39feda7b0c8c1384328659ff6ac13e27d6
Mark.thebault <mark.thebault+github@gmail.com>
2014-10-08 19:07:01 +0200

    ajout un nom a uneclasse

--- src/UneClasse.java
+++ src/UneClasse.java
@@ -1,7 +1,18 @@
 
 public class UneClasse {
+	
+	private String Name;
 
-	public UneClasse() {
-		// TODO Auto-generated constructor stub
+	public UneClasse(String name) {
+		this.setName(name);
 	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+	
 }
